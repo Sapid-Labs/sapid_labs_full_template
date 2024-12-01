106cef412d33f43da806d2287026696c40beb85a
Joseph Muller <jtmuller5@gmail.com>
2024-12-01 10:51:03 -0500

    Add Fastlane files

---
+++ lib/features/shared/ui/app_version.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+class AppVersion extends StatelessWidget {
+  const AppVersion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: PackageInfo.fromPlatform(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final PackageInfo packageInfo = snapshot.data as PackageInfo;
+          return Text(
+            'Version: ${packageInfo.version}',
+            style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          );
+        }
+
+        return const SizedBox();
+      },
+    );
+  }
+}
