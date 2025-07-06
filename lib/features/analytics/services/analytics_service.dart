0b1330a607b812b392e7a7a7086954d8a4dccb06
Joseph Muller <jtmuller5@gmail.com>
2025-07-06 19:02:29 -0400

    Update analytics_service.dart

--- lib/features/analytics/services/analytics_service.dart
+++ lib/features/analytics/services/analytics_service.dart
@@ -1,19 +1,24 @@
 import 'package:slapp/features/analytics/services/amplitude_analytics_service.dart';
+import 'package:slapp/features/analytics/services/firebase_analytics_service.dart';
 
 abstract class AnalyticsService {
   Future<void> setup() async {
     AmplitudeAnalyticsService().setup();
+    FirebaseAnalyticsService().setup();
   }
 
   Future<void> setUserProperties(Map<String, dynamic> properties) async {
     AmplitudeAnalyticsService().setUserProperties(properties);
+    FirebaseAnalyticsService().setUserProperties(properties);
   }
 
   Future<void> logEvent(String name, {Map<String, dynamic>? parameters}) async {
     AmplitudeAnalyticsService().logEvent(name, parameters: parameters);
+    FirebaseAnalyticsService().logEvent(name, parameters: parameters);
   }
 
   void updateVersionId(String? versionId, {String? userId}) {
     AmplitudeAnalyticsService().updateVersionId(versionId);
+    FirebaseAnalyticsService().updateVersionId(versionId);
   }
 }
