0348f851248a056ba945d280755ad675e7c4455b
Joseph Muller <jtmuller5@gmail.com>
2025-07-07 09:23:51 -0400

    Snippets

---
+++ lib/features/rss/ui/rss_view.dart
@@ -0,0 +1,18 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class RssView extends StatefulWidget {
+  const RssView({super.key});
+
+  @override
+  State<RssView> createState() => _RssViewState();
+}
+
+class _RssViewState extends State<RssView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
